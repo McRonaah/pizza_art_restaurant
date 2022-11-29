43e5a15a9348535c9e7100cf4b479521709d3bf8
McRonaah <ronaldcheruiyot342@gmail.com>
2022-11-29 12:47:10 +0300

    updated routes changes

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,33 +1,13 @@
 class RestaurantPizzasController < ApplicationController
-    rescue_from ActiveRecord::RecordInvalid, with: :unprocessable_entity_method
-    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
-    #POST new restaurant_pizza
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
     def create
-        restaurant_pizza = RestaurantPizza.create!(restaurant_pizza_params)
-        #returns the new entity and status
-        render json: restaurant_pizza, status: :created
-    end
-    #Get all the restaurant_pizzas
-    def index
-        restaurant_pizzas = RestaurantPizza.all
-        render json: restaurant_pizzas
-    end
-    #GET restaurant_pizza/:id
-    def show
-        restaurant_pizza = find_restaurant_pizza
-        render json: restaurant_pizza
+        pizza = Pizza.find(params[:pizza_id])
+        restaurant_pizza = RestaurantPizza.create!(restaurant_params)
+        render json: pizza
+    rescue ActiveRecord::RecordInvalid => invalid
+        render json: { errors: invalid.record.errors.full_messages }, status: :unprocessable_entity
     end
     private
-    def restaurant_pizza_params
-        params.permit(:price, :pizza_id, :restaurant_id)
-    end
-    def unprocessable_entity_method entity
-        render json: {errors: entity.record.errors}, status: :unprocessable_entity
-    end
-    def find_restaurant_pizza
-        RestaurantPizza.find_by!(id: params[:id])
-    end
-    def record_not_found
-        render json: {error: "RestaurantPizza Not Found"}, status: :not_found
-    end
+    def restaurant_params
+        params.permi
 end
